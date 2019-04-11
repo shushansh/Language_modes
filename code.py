462f48f6b3984d92fd79b2ee5e06441bb2a7a306
shushansh <guptashushansh@gmail.com>
2019-04-11 23:11:36 +0530

    Updated I/O formats

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-import PyPDF4
 import textract
 import re
 import nltk
@@ -7,7 +6,7 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.stem import PorterStemmer
 
-file = 'path/to/file'
+file = str(input("File location : "))
 
 pst = PorterStemmer()
 
@@ -47,19 +46,20 @@ if len(rqd_text) < 1:
     print("Error! Data not found.")
     exit(1)
 
-clean_text = rqd_text[0].replace(',','').replace(';','')
+clean_text = rqd_text[0].replace(',','').replace(';','').rstrip()
+clean_text = clean_text.replace('\n','').replace('EOL', '')
 clean_text_split = word_tokenize(clean_text)
-
-print(clean_text)
 
 amount = re.findall(r'-?\d+\.?\d*', clean_text)
 amount_matches = []
-for intl in amount:
-    if float(intl) > 10:
+previntl = ''
+for intl in clean_text_split:
+    if previntl in currencies_check:
         amount_matches.append(float(intl))
+    previntl = intl
 
-if re.search('(.+?)elivery(.+?)and(.+?)eturn(.+?)', clean_text):
-    m = re.search('(.+?)up(.+?)and', clean_text)
+if re.search('''(.+?)elivery(.+?)and(.+?|\n|\r|\r\n)eturn(.+?)''', clean_text):
+    m = re.search('''(.+?)up(.+?)and''', clean_text)
     if m:
         delivery_round = 'up'
         return_round = 'down'
@@ -67,10 +67,10 @@ if re.search('(.+?)elivery(.+?)and(.+?)eturn(.+?)', clean_text):
         delivery_round = 'down'
         return_round = 'up'
 else:
-    if re.search('up(.+?)and(.+?)', clean_text):
+    if re.search('''up(.+?)and(.+?)''', clean_text):
         delivery_round = 'up'
         return_round = 'down'
-    if re.search('down(.+?)eturn(.+?)up', clean_text):
+    if re.search('''down(.+?)eturn(.+?)up''', clean_text):
         delivery_round = 'down'
         return_round = 'up'
     if 'up' in clean_text_split and 'down' not in clean_text_split:
@@ -92,5 +92,5 @@ else:
     return_amount = amount_matches[0]
 
 
-print("Delivery Amount:\nCurrency: "+currency+"\nAmount: "+str(delivery_amount)+"\nRound: "+delivery_round+"\n")
-print("Return Amount:\nCurrency: "+currency+"\nAmount: "+str(return_amount)+"\nRound: "+return_round+"\n")
+print("Delivery Amount:\nCurrency: "+currency+"\nAmount: {:0,.0f}".format(delivery_amount)+"\nRound: "+delivery_round+"\n")
+print("Return Amount:\nCurrency: "+currency+"\nAmount: {:0,.0f}".format(return_amount)+"\nRound: "+return_round+"\n")
